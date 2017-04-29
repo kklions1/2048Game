9316464353c1b7d8628a3688144d8150f8cd58e9
Kevin <kklions1@gmail.com>
2017-04-28 22:00:30 -0400

    made helpMenu extend Frame, instead of having it make a separate instance of frame inside the class

--- src/kevin/HelpMenu.java
+++ src/kevin/HelpMenu.java
@@ -4,9 +4,7 @@ import java.awt.*;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
-class HelpMenu {
-
-    private Frame helpWindow;
+class HelpMenu extends Frame {
 
     HelpMenu() {
 
@@ -15,24 +13,24 @@ class HelpMenu {
         Label helpTextLine3;
         Label helpTextLine2;
 
-        helpWindow = new Frame("Help");
-        helpWindow.setSize(300,150);
+        setTitle("Help");
+        setSize(300,150);
 
 
         helpTextLine1 = new Label("Controls:");
         helpTextLine2 = new Label("W = up, A = left, S = down, D = right");
         helpTextLine3 = new Label("Written by Kevin Klions");
 
-        helpWindow.setLayout(new FlowLayout());
+        setLayout(new FlowLayout());
 
-        helpWindow.add(helpTextLine1);
-        helpWindow.add(helpTextLine2);
-        helpWindow.add(helpTextLine3);
+        add(helpTextLine1);
+        add(helpTextLine2);
+        add(helpTextLine3);
 
-        helpWindow.setVisible(true);
+        setVisible(true);
 
-        helpWindow.addWindowListener(new WindowAdapter() {
-            public void windowClosing(WindowEvent we) { helpWindow.setVisible(false); }
+        addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent we) { setVisible(false); }
         });
 
 
--- src/kevin/Main.java
+++ src/kevin/Main.java
@@ -14,4 +14,10 @@ public class Main {
 
 
 	}
+
+
+
+
+
+
 }	
